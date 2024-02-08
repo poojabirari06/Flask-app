1115a01a5f92704605f2fa43841c56d394584aff
poojabirari06 <155713342+poojabirari06@users.noreply.github.com>
2024-02-08 11:26:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,5 +41,5 @@ def delete_note(note_id):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
 
